af859b0fa512d517d2c53f67855122d0f0a8e76c
Chris Piggott <cpiggott@ksu.edu>
2015-10-07 15:39:58 -0500

    Adding info from class

---
+++ day2/Complete/ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Chris Piggott'
+my_age = 24
+my_height = 70.5 # inches
+my_weight = 220 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+
+print "Let's talk about %s." % my_name
+print "He's %f inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "He's %d years old." % my_age
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+
+print "If I add %d, %d, and %d I get %d." % (
+    my_age, my_height, my_weight, my_age + my_height + my_weight)
